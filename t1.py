03dd07ac193cf82828d616ba7e599629c7bc2dd0
Intmeg <162458283+Intmeg@users.noreply.github.com>
2024-03-06 18:08:23 +0300

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,8 +1,8 @@
-f = open(r'C:\Users\TEMP.DT.001\PycharmProjects\predprof14\products.txt')
+f = open(r'.\products.txt')
 k = 0
 for a in f:
     s = ''.join(a)
-    x = open(r'C:\Users\TEMP.DT.001\PycharmProjects\predprof14\products_new.csv')
+    x = open(r'.\products_new.csv')
     x[k].append(s)
     k += 1
     x.close()
